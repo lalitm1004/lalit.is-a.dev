d886609672d8abc05dcadff56f59b2c3f5611ff2
Lalit M <lalitm1004@gmail.com>
2024-02-25 12:52:21 +0530

    Initial commit

---
+++ app/projects/page.js
@@ -0,0 +1,9 @@
+export default function Projects() {
+  return (
+    <main>
+      <div className="bg-off-black h-screen w-screen flex justify-center items-center">
+        Gotta study for midsem exams, will make later :P
+      </div>
+    </main>
+  )
+}
