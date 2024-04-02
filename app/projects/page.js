d8668d761ac0d4cff3b920b860ed5fbfe8bda79c
Lalit M <lalitm1004@gmail.com>
2024-04-02 22:33:43 +0530

    Delete all

--- app/projects/page.js
+++
@@ -1,9 +0,0 @@
-export default function Projects() {
-  return (
-    <main>
-      <div className="bg-off-black h-screen w-screen flex justify-center items-center">
-        Gotta study for midsem exams, will make later :P
-      </div>
-    </main>
-  )
-}
